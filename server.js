b9c18b22dba2fcd5a77c720da56982355cd6960b
Tamilore Akinsola <tammy@Tamilores-MacBook-Air.local>
2025-08-07 16:55:10 +0100

    feat: Second Update before Deployment

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
 const allowedOrigins = [
+  process.env.FRONTEND_URL,
   'http://localhost:3000',
   // 'ws://localhost:3000',
   process.env.FRONTEND_URL
